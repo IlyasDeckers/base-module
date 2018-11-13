fb70b2dd5e02a816ddb4e35aa2ee0c8495721c3c
Ilyas Deckers <deckersilyas@gmail.com>
2018-11-13 20:51:34 +0100

    rm callsatic

--- src/Traits/CallStatic.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Clockwork\Base\Traits;
-
-trait CallStatic {
-    public static function __callStatic($method, $parameters)
-    {
-        return (new static)->$method(...$parameters);
-    }
-}
