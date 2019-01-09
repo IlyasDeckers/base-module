4638c75a5131a3dc35512d0220d3561baf07e89b
Ilyas Deckers <deckersilyas@gmail.com>
2019-01-09 10:08:14 +0100

    Add controllers for ACL

---
+++ src/Auth/Http/Resources/PermissionResource.php
@@ -0,0 +1,20 @@
+<?php
+namespace Clockwork\Base\Auth\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class PermissionResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name
+        ];
+    }
+}
---
+++ src/Auth/Http/Resources/RoleResource.php
@@ -0,0 +1,23 @@
+<?php
+namespace Clockwork\Base\Auth\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class RoleResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'guard_name' => $this->guard_name,
+            'permissions' => PermissionResource::collection($this->whenLoaded('permissions'))
+        ];
+    }
+}
