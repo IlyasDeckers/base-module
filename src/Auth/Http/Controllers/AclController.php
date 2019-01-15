871717920246a1bf858872a9f3250f7600ea8e62
Ilyas Deckers <deckersilyas@gmail.com>
2019-01-15 11:25:47 +0100

    create ACL controller

---
+++ src/Auth/Http/Controllers/AclController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Clockwork\Base\Auth\Http\Controllers;
+
+use Clockwork\Base\Auth\Resources\RoleResource;
+use Clockwork\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class AclController extends Controller
+{
+    /**
+     * List roles
+     *
+     * @param  Request request
+     */
+    public function assignPermissions(Request $request)
+    {
+        $role = Role::findOrFail($request->id);
+        $permissions = $role->permissions;
+
+        $permissions->each(function ($permission) use ($role) {
+            $role->revokePermissionTo($permission->name);
+        });
+
+        foreach ($request->permissions as $permission) {
+            $role->givePermissionTo($permission['name']);
+        }
+
+        return new RoleResource(
+            Role::with('permissions')->findOrFail($request->id)
+        );
+    }
+}
