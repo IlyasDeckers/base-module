2e8875e317c622940ea26b4b21ba76d762f2ae2f
IlyasDeckers <deckersilyas@gmail.com>
2019-12-11 15:03:39 +0100

    add audit model

---
+++ src/Models/Audit.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Clockwork\Base\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Audit extends Model implements \OwenIt\Auditing\Contracts\Audit
+{
+    use \OwenIt\Auditing\Audit;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $guarded = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $casts = [
+        'old_values'   => 'json',
+        'new_values'   => 'json',
+        'auditable_id' => 'integer',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(\Clockwork\Users\Models\User::class, 'user_id');
+    }
+
+    public function auditable()
+    {
+        return $this->morphTo('auditable');
+    }
+
+    public function scopeFromManager($query)
+    {
+        return $query->whereHas('user', function ($q) {
+            return $q->where('role', 'manager');
+        });
+    }
+
+    public function scopeByMonth($query)
+    {
+        return $query->where('created_at', '<=', request()->end)
+            ->where('created_at', '>=', request()->start);
+    }
+}
