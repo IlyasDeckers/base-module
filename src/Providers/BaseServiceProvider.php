f313e714c1acffd3e686c554fa00510f437c35ae
IlyasDeckers <deckersilyas@gmail.com>
2019-12-11 14:36:56 +0100

    implement auto factory generation

---
+++ src/Providers/BaseServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Clockwork\Base\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Clockwork\Base\Console\Commands\GenerateFactoriesCommand;
+
+class BaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $viewPath = __DIR__.'/../resources/views';
+        $this->loadViewsFrom($viewPath, 'test-factory-helper');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('command.test-factory-helper.generate',
+            function ($app) {
+                return new GenerateFactoriesCommand($app['files'], $app['view']);
+            }
+        );
+        $this->commands('command.test-factory-helper.generate');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('command.test-factory-helper.generate');
+    }
+}
