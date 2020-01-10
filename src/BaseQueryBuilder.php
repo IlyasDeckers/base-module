feed2567ce14ff806dfd3624486732b7b54ffbad
IlyasDeckers <deckersilyas@gmail.com>
2020-01-10 15:54:47 +0100

    implement BaseQueryBuilder

--- src/BaseQueryBuilder.php
+++ src/BaseQueryBuilder.php
@@ -1,50 +1,11 @@
 <?php
 namespace Clockwork\Base;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Http\Request;
 
-abstract class BaseRepository extends BaseQueryBuilder implements BaseRepositoryInterface
+abstract class BaseQueryBuilder implements BaseQueryBuilderInterface
 {
-    protected $model;
-
-    // Deprecated??
-    protected Request $request;
-
-    abstract public function store(Request $request) : object;
-
-    abstract public function update(array $data)  : object;
-
-    abstract public function destroy(int $id) : void;
-
-    /**
-     * Get a item from the database
-     *
-     * @param object $request
-     * @return item
-     */
-    public function find(Request $request) : object
-    {
-        return $this->itemResponse(
-            $this->model->where('id', $request->id)
-        );
-    }
-
-    /**
-     * Get a collection from the database
-     *
-     * @param object $request
-     * @return collection
-     */
-    public function getAll(Request $request) : Collection  
-    {   
-        return $this->collectionResponse(
-            $this->model
-        );
-    }
-
     /**
      * Get an item and it's relations
      *
@@ -52,12 +13,10 @@ abstract class BaseRepository extends BaseQueryBuilder implements BaseRepository
      * @param [type] $query
      * @return collection
      */
-    public function itemResponse(Request $request, object $query) : object
+    public function itemResponse(object $query) : object
     {
-        $this->request = $request;
-
         return $query
-            ->when($request->has('with'), [$this, 'with'])
+            ->when(request()->has('with'), [$this, 'with'])
             ->first();
     }
 
@@ -69,26 +28,24 @@ abstract class BaseRepository extends BaseQueryBuilder implements BaseRepository
      * @param object $query
      * @return collection
      */
-    public function collectionResponse(Request $request, object $query) : Collection
+    public function collectionResponse(object $query) : Collection
     {
-        $this->request = $request;
-
         $query = $query
-            ->when($request->has('filter'), [$this, 'filter'])
-            ->when($request->has('with'), [$this, 'with'])
-            ->when($request->has('scopes'), [$this, 'scopes'])
+            ->when(request()->has('filter'), [$this, 'filter'])
+            ->when(request()->has('with'), [$this, 'with'])
+            ->when(request()->has('scopes'), [$this, 'scopes'])
             ->when(
-                $request->has('sort') && $request->sort !== null,
+                request()->has('sort') && $request->sort !== null,
                 /* if */   [$this, 'sort'],
                 /* else */ [$this, 'noSort']
             );
 
-        if ($request->paginate == 'true') {
-            return $query->paginate($request->per_page);
+        if (request()->paginate == 'true') {
+            return $query->paginate(request()->per_page);
         }
 
         return $query->get()
-            ->when($request->has('groupBy'), [$this, 'groupBy']);
+            ->when(request()->has('groupBy'), [$this, 'groupBy']);
     }
 
     /**
@@ -99,7 +56,9 @@ abstract class BaseRepository extends BaseQueryBuilder implements BaseRepository
      */
     public function filter(Builder $query) : Builder
     {
-        return $query->search($this->request->filter);
+        return $query->search(
+            request()->filter
+        );
     }
 
     /**
@@ -111,7 +70,7 @@ abstract class BaseRepository extends BaseQueryBuilder implements BaseRepository
     public function with(Builder $query) : Builder
     {
         return $query->with(
-            explode(',', $this->request->with)
+            explode(',', request()->with)
         );
     }
 
@@ -123,13 +82,13 @@ abstract class BaseRepository extends BaseQueryBuilder implements BaseRepository
      */
     public function scopes(Builder $query) : Builder
     {
-        $decoded = json_decode($this->request->scopes, true);
+        $decoded = json_decode(request()->scopes, true);
 
         if (json_last_error() == JSON_ERROR_NONE) {
-            dd($decoded); // If you encounter this line, open a issue and provide the output please :)
+            dd($decoded);
             $scopes = $decoded;
         } elseif (!is_object($decoded) && !is_array($decoded)) {
-            $scopes = explode(',', $this->request->scopes);
+            $scopes = explode(',', request()->scopes);
         } else {
             $scopes = [];
         }
@@ -145,7 +104,7 @@ abstract class BaseRepository extends BaseQueryBuilder implements BaseRepository
      */
     public function sort(Builder $query) : Builder
     {
-        foreach (explode(',', $this->request->sort) as $sort) {
+        foreach (explode(',', request()->sort) as $sort) {
             list($sortCol, $sortDir) = explode('|', $sort);
             $query = $query->orderBy($sortCol, $sortDir);
         }
@@ -172,6 +131,6 @@ abstract class BaseRepository extends BaseQueryBuilder implements BaseRepository
      */
     public function groupBy(Builder $query) : Builder
     {
-        return $query->groupBy($this->request->groupBy);
+        return $query->groupBy(request()->groupBy);
     }
 }
